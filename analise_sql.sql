31ffc2ab08b21b0d9d1d349804659aa506450eac
Carolyne Oliver <66889140+carolyneoliverr@users.noreply.github.com>
2024-02-25 19:44:46 -0300

    analise_sql.sql

--- analise_sql.sql
+++ analise_sql.sql
@@ -1,3 +1,5 @@
+## Localização de chamados do 1746 , Utilizando a tabela de Chamados do 1746 e a tabela de Bairros do Rio de Janeiro para as perguntas de 1-5. ##
+
 -- Questão 1: Quantos chamados foram abertos no dia 01/04/2023? R: Foram abertos 65938 chamados.
 SELECT COUNT(*) AS total
 FROM datario.administracao_servicos_publicos.chamado_1746
@@ -39,4 +41,45 @@ SELECT c.id_bairro, c.data_particao, c.tipo, c.latitude, c.longitude, b.nome AS 
   WHERE b.id_bairro IS NULL
   AND c.data_particao = '2023-04-01'
   LIMIT 100;
+
+
+## Chamados do 1746 em grandes eventos, utilizando a tabela de Chamados do 1746 e a tabela de Ocupação Hoteleira em Grandes Eventos no Rio para as perguntas de 6-10. ##
+
+--Questão 6: Quantos chamados com o subtipo "Perturbação do sossego" foram abertos desde 01/01/2022 até 31/12/2023 (incluindo extremidades)? R: 42408
+SELECT COUNT(*) AS total
+FROM datario.administracao_servicos_publicos.chamado_1746 AS c
+WHERE c.data_particao BETWEEN '2022-01-01' AND '2023-12-31'
+AND c.subtipo = 'Perturbação do sossego';
+
+--Questão 7: Selecione os chamados com esse subtipo que foram abertos durante os eventos contidos na tabela de eventos (Reveillon, Carnaval e Rock in Rio). R:
+SELECT c.id_chamado, c.data_inicio, c.subtipo, c.id_bairro, e.evento
+FROM datario.administracao_servicos_publicos.chamado_1746 AS c
+INNER JOIN datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos AS e
+ON c.data_particao BETWEEN e.data_inicial AND e.data_final
+WHERE c.subtipo = 'Perturbação do sossego'
+AND e.evento IN ('Reveillon', 'Carnaval', 'Rock in Rio');
+
+-- Questão 8: Quantos chamados desse subtipo foram abertos em cada evento? R: Reveillon (79), Carnaval (197), Rock in Rio (518)
+SELECT e.evento, COUNT(*) AS total_chamados
+FROM datario.administracao_servicos_publicos.chamado_1746 AS c
+INNER JOIN datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos AS e
+ON c.data_inicio BETWEEN e.data_inicial AND e.data_final
+WHERE c.subtipo = 'Perturbação do sossego'
+AND e.evento IN ('Reveillon', 'Carnaval', 'Rock in Rio')
+GROUP BY e.evento;
+
+-- Questão 9: Qual evento teve a maior média diária de chamados abertos desse subtipo? R: Reveillon com 2129 chamados abertos.
+SELECT evento, AVG(total_chamados_dia) AS media_diaria
+FROM (
+  SELECT e.evento, COUNT(*) AS total_chamados_dia
+  FROM datario.administracao_servicos_publicos.chamado_1746 AS c
+  INNER JOIN datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos AS e
+  ON c.data_particao BETWEEN e.data_inicial AND e.data_final
+  WHERE c.subtipo = 'Perturbação do sossego'
+  AND e.evento IN ('Reveillon', 'Carnaval', 'Rock in Rio')
+  GROUP BY e.evento, c.data_particao
+) AS t
+GROUP BY evento;
+
+-- Questão 10: Compare as médias diárias de chamados abertos desse subtipo durante os eventos específicos (Reveillon, Carnaval e Rock in Rio) e a média diária de chamados abertos desse subtipo considerando todo o período de 01/01/2022 até 31/12/2023. R:
 
